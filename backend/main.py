acd028a07cdbde399852f65a07c2ec7237dd1f5e
Jefferson Meneses <jeffersonmeneses98@gmail.com>
2022-10-10 21:40:54 -0300

    Add talkback

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,32 @@
 from vosk import Model, KaldiRecognizer
 import os
 import pyaudio
+import json
 
+import tts.tts as tts
+
+
+# TTS - Text to Speech
+#text = "Olá, meu nome é Istáius. No que posso te ajudar?"
+#tts.speak(text)
+
+# ASR - Automated Speech Recognition 
 model = Model("model")
 rec = KaldiRecognizer(model, 16000)
 
 p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=2048)
 stream.start_stream()
 
 while True:
-    data = stream.read(4000)
+    data = stream.read(2048)
     if len(data) == 0:
         break
     if rec.AcceptWaveform(data):
-        print(rec.Result())
-    else:
-        print(rec.PartialResult())
-print(rec.FinalResult())
+        result = rec.Result()
+        result = json.loads(result)
+
+        if result is not None:
+            text = result['text']
+            print(text)
+            tts.speak(text)
